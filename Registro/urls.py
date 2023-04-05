a7a1a871f4148cb352380c735eaf757aa6b94e83
Gianfranco Valente Cano <gianny@MBPdeGianfranco.lan>
2023-04-05 11:21:31 -0300

    Checkpoint. Finalizacion de renderizacion. ahora vamos a conectarla con Google.

--- Registro/urls.py
+++ Registro/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, profile
 
 urlpatterns = [
-   path('', home, name='home')
+   path('', home, name='home'),
+   path('accounts/profile/', profile, name='profile' ),
 ]
--- Registro/views.py
+++ Registro/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render (request, 'Registro/home.html')
+
+def profile(request):
+    return render(request, 'account/profile.html')
